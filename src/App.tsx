7b84e629455518f8173104529738d1ecb85f28c7
Pearu <bartekg100@gmail.com>
2024-10-08 19:32:40 +0000

    setup daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,15 @@ import React from "react";
 
 
 const App = () => {
-  const name = "John";
-
-  const names: Array<string> = ["test1", "test2", "test3"];
-
-  const isLogged = true;
-
   return (
-    <div className="bg-zinc-900 w-100 h-screen text-white">
-      <h1 className="text-3xl font-bold">Hello world! {name}</h1>
-      <ul>
-        {names.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-      {isLogged ? <h1 className="text-3xl font-bold">Test</h1> : ""}
+    <div className="flex space-x-3">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
     </div>
   );
 };
