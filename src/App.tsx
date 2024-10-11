40581d009a0c225d7761a250201dbd19a3fcdc84
Bartek Górka <bgorka@kofio.pl>
2024-10-11 22:07:35 +0200

    Set tabs in separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,14 @@
 import React from "react";
 import NavBar from "./components/common/NavBar";
-import NavBarItem from "./models/NavBarItem";
+import tabs from "./data/tabs"
 
 
 const App = () => {
-  const sampleNavLinks: NavBarItem[] = [
-    new NavBarItem({
-      Title: "Home",
-      SubItems: [
-        new NavBarItem({ Title: "Dashboard" }),
-        new NavBarItem({ Title: "Analytics" }),
-      ],
-    }),
-    new NavBarItem({
-      Title: "About",
-      SubItems: [
-        new NavBarItem({ Title: "Team" }),
-        new NavBarItem({ Title: "Company" }),
-      ],
-    }),
-    new NavBarItem({
-      Title: "Services",
-      SubItems: [
-        new NavBarItem({ Title: "Web Development" }),
-        new NavBarItem({ Title: "App Development" }),
-      ],
-    }),
-    new NavBarItem({
-      Title: "Services",
-    }),
-  ];
 
 
   return (
     <div>
-      <NavBar title="Animation App" links={sampleNavLinks}/>
+      <NavBar title="Animation App" links={tabs}/>
       
     </div>
   );
---
+++ src/data/tabs.ts
@@ -0,0 +1,30 @@
+import NavBarItem from '../models/NavBarItem';
+
+const tabs: NavBarItem[] = [
+    new NavBarItem({
+        Title: "Basic Transitions",
+        SubItems: []
+    }),
+    new NavBarItem({
+        Title: "Gestures & Interactions",
+        SubItems: []
+    }),
+    new NavBarItem({
+        Title: "Scroll Animations",
+        SubItems: []
+    }),
+    new NavBarItem({
+        Title: "Spring Animations",
+        SubItems: []
+    }),
+    new NavBarItem({
+        Title: "Chain Animations",
+        SubItems: []
+    }),
+    new NavBarItem({
+        Title: "Use Cases",
+        SubItems: []
+    })
+];
+
+export default tabs;
